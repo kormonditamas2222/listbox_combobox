ec3be4f34c97ba452e2b9bae70f15b6604a2cb78
kormonditamas2222 <t.kormond2006@gmail.com>
2025-09-17 09:23:24 +0200

    nagyon listbox

--- listboxok/MainWindow.xaml.cs
+++ listboxok/MainWindow.xaml.cs
@@ -16,12 +16,26 @@ namespace listboxok
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> elemek = ["alma", "banán", "körte"];
         public MainWindow()
         {
             InitializeComponent();
-            List<string> elemek = ["alma", "banán", "körte"];
             lbox_1.ItemsSource = elemek;
         }
 
+        private void btn_1_Click(object sender, RoutedEventArgs e)
+        {
+            string ujElem = tb_1.Text.Trim();
+            if (!string.IsNullOrEmpty(ujElem)) 
+            { 
+                elemek.Add(ujElem);
+                lbox_1.Items.Refresh();
+                tb_1.Clear();
+            }
+            else
+            {
+                MessageBox.Show(this, "A mezőt kötelező kitölteni!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
     }
 }
